deda6994341793a1a7d16801504d6d4a42ad7c44
Christopher Kolstad <git@chriswk.no>
2020-05-21 22:13:59 +0200

    Mix format

--- lib/squash_web/live/referee_live.ex
+++ lib/squash_web/live/referee_live.ex
@@ -2,8 +2,6 @@ defmodule SquashWeb.RefereeLive do
   use SquashWeb, :live_view
 
   def mount(%{"matchid" => matchid}, %{}, socket) do
-
     {:ok, assign(socket, player1score: 0, player2score: 0, matchid: matchid)}
   end
-
 end
