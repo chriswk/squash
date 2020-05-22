9ef5d6275d746b793cc1a9fccd223c9098c4a841
Christopher Kolstad <git@chriswk.no>
2020-05-22 10:06:14 +0200

    Setup csrf origin

--- config/releases.exs
+++ config/releases.exs
@@ -3,4 +3,5 @@ import Config
 config :squash, SquashWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  check_origin: ["https://squash.chriswk.com", System.get_env("APP_NAME") <> ".gigalixirapp.com"]
