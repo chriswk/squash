4e2686ed4f1c8cdfc00f8f905a985821edacc155
Christopher Kolstad <git@chriswk.no>
2020-05-22 09:58:22 +0200

    Tweak release config

--- config/releases.exs
+++ config/releases.exs
@@ -1,6 +1,6 @@
 import Config
 
-config :c, SquashWeb.Endpoint,
+config :squash, SquashWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
