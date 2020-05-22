21aa081c62f5886756bc8417bed3e55cb36bcd30
Christopher Kolstad <git@chriswk.no>
2020-05-22 22:38:59 +0200

    Can now update two counters

--- lib/squash_web/live/referee_live.ex
+++ lib/squash_web/live/referee_live.ex
@@ -5,4 +5,20 @@ defmodule SquashWeb.RefereeLive do
   def mount(%{"matchid" => matchid}, %{}, socket) do
     {:ok, assign(socket, player1score: 0, player2score: 0, matchid: matchid)}
   end
+
+  def handle_event("incp1", _, socket) do
+    {:noreply, assign(socket, player1score: socket.assigns.player1score + 1)}
+  end
+
+  def handle_event("decp1", _, socket) do
+    {:noreply, assign(socket, player1score: max(0, socket.assigns.player1score - 1))}
+  end
+
+  def handle_event("incp2", _, socket) do
+    {:noreply, assign(socket, player2score: socket.assigns.player2score + 1)}
+  end
+
+  def handle_event("decp2", _, socket) do
+    {:noreply, assign(socket, player2score: max(0, socket.assigns.player2score - 1))}
+  end
 end
